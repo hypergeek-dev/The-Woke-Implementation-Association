398f4c5d019e082094d0194723741d411a11b360
Tinks18 <tinutani9@gmail.com>
2023-02-17 02:32:45 +0000

    setting up base.html and routing

--- run.py
+++ run.py
@@ -10,6 +10,21 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/map")
+def map():
+    return render_template("map.html")
+
+
+@app.route("/donate")
+def donate():
+    return render_template("donate.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
